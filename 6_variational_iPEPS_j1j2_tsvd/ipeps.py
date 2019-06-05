correct normalization for V in arnoldi svd solver

6_variational_iPEPS_j1j2_tsvd/ipeps.py
@@ -16,6 +16,7 @@ class iPEPS(torch.nn.Module):
         self.chi = args.chi
         self.tsvd_extra = args.tsvd_extra
         self.Niter = args.Niter
+        self.threshold = args.ctm_threshold
         self.use_checkpoint = use_checkpoint 
         
         d, D = self.d, self.D
@@ -40,7 +41,7 @@ class iPEPS(torch.nn.Module):
         T = T.permute(0,4, 1,5, 2,6, 3,7).contiguous().view(D**2, D**2, D**2, D**2)
         T = T/T.norm()
 
-        C, E = CTMRGTSVD(T, chi, tsvd_extra, Niter, self.use_checkpoint) 
+        C, E = CTMRGTSVD(T, chi, tsvd_extra, Niter, self.threshold, self.use_checkpoint) 
         loss, Mx, My, Mz = get_obs(Asymm, H, Mpx, Mpy, Mpz, C, E)
 
         return loss, Mx, My, Mz 

tensornets/adlib/arnoldisvd.py
@@ -5,7 +5,7 @@ We reimplement it with a safe inverse function in light of degenerated singular 
 
 import numpy as np
 import torch
-import torch.nn.functional as F
+import torch.nn.functional as Functional
 import scipy.sparse.linalg
 
 def safe_inverse(x, epsilon=1E-12):
@@ -59,14 +59,18 @@ class ARNOLDISVD(torch.autograd.Function):
         # compute right singular vectors as Mt = V.S.Ut /.U => Mt.U = V.S
         # since M = Mt, M.U = V.S
         V = M @ U
-        V = F.normalize(V, p=0.5, dim=1)
+        V = Functional.normalize(V, p=2, dim=0)
 
-        #print("U "+str(U.shape))
-        #print(U.stride())
+        # print("U "+str(U.shape))
+        # print(U.stride())
+        # for i in range(k):
+        #     print(str(i)+": "+str(np.linalg.norm(U[:,i])))
         #print(S.shape)
         #print(S)
-        #print("V "+str(V.shape))
-        #print(V.stride())
+        # print("V "+str(V.shape))
+        # print(V.stride())
+        # for i in range(k):
+        #     print(str(i)+": "+str(np.linalg.norm(V[:,i])))
 
         self.save_for_backward(U, S, V)
         return U, S, V

tensornets/ctmrg.py
@@ -3,8 +3,8 @@ from torch.utils.checkpoint import checkpoint
 
 from .adlib import SVD 
 svd = SVD.apply
-#from .adlib import EigenSolver
-#symeig = EigenSolver.apply
+
+
 
 def renormalize(*tensors):
     # T(up,left,down,right), u=up, l=left, d=down, r=right

tensornets/ctmrgtsvd.py
@@ -1,10 +1,10 @@
 import torch
 from torch.utils.checkpoint import checkpoint
 
-from .adlib import RSVD 
-svd = RSVD.apply
-# from .adlib import ARNOLDISVD 
-# svd = ARNOLDISVD.apply
+#from .adlib import RSVD 
+#svd = RSVD.apply
+from .adlib import ARNOLDISVD 
+svd = ARNOLDISVD.apply
 #from .adlib import EigenSolver
 #symeig = EigenSolver.apply
 
@@ -18,19 +18,41 @@ def renormalize(*tensors):
     D_new = min(dimE*dimT, int(chi))
 
     # step 1: contruct the density matrix Rho
+    # C--1 0--E--2->2
+    # |0      |1
     Rho = torch.tensordot(C,E,([1],[0]))        # C(ef)*EU(fga)=Rho(ega)
+    # C----E--2->1
+    # |0   |1->0
+    # |0 
+    # E--1->2
+    # |2->3
     Rho = torch.tensordot(Rho,E,([0],[0]))      # Rho(ega)*EL(ehc)=Rho(gahc)
+    # C-------E--1->0
+    # |       |0
+    # |       |0
+    # E--2 1--T--3->3
+    # |3->1   |2->2
     Rho = torch.tensordot(Rho,T,([0,2],[0,1]))  # Rho(gahc)*T(ghdb)=Rho(acdb)
+    # C--03->01
+    # |
+    # 12->34
     Rho = Rho.permute(0,3,1,2).contiguous().view(dimE*dimT, dimE*dimT)  # Rho(acdb)->Rho(ab;cd)
 
     Rho = Rho+Rho.t()
     Rho = Rho/Rho.norm()
 
+    # C--E-- => S--U 
+    # |  |   => |
+    # |  |   => V^dag
+    # E--T-- =>
+    # |  |   =>
+
     # step 2: Get Isometry P
     U, S, V = svd(Rho,D_new+int(tsvd_extra))
     truncation_error = S[D_new:].sum()/S.sum()
     P = U[:, :D_new] # projection operator
-    
+    #Pt = V[:, :D_new]
+
     #can also do symeig since Rho is symmetric 
     #S, U = symeig(Rho)
     #sorted, indices = torch.sort(S.abs(), descending=True)
@@ -43,6 +65,7 @@ def renormalize(*tensors):
 
     ## EL(u,r,d)
     P = P.view(dimE,dimT,D_new)
+    #Pt = Pt.view(dimE,dimT,D_new)
     E = torch.tensordot(E, P, ([0],[0]))  # EL(def)P(dga)=E(efga)
     E = torch.tensordot(E, T, ([0,2],[1,0]))  # E(efga)T(gehb)=E(fahb)
     E = torch.tensordot(E, P, ([0,2],[0,1]))  # E(fahb)P(fhc)=E(abc)
@@ -54,10 +77,12 @@ def renormalize(*tensors):
     return C/C.norm(), E, S.abs()/S.abs().max(), truncation_error
 
 
-def CTMRGTSVD(T, chi, tsvd_extra, max_iter, use_checkpoint=False):
+def CTMRGTSVD(T, chi, tsvd_extra, max_iter, thresh=None, use_checkpoint=False):
     # T(up, left, down, right)
 
     threshold = 1E-8 if T.dtype is torch.float64 else 1E-6 # ctmrg convergence threshold
+    if thresh is not None:
+        threshold = thresh
 
     # C(down, right), E(up,right,down)
     C = T.sum((0,1))  #